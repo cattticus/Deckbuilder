12d91a8047c3e2a63e281688a8cc5b3a83a6f5c8
cat <c.finley663@mybvc.ca>
2025-02-24 15:32:18 -0700

    basic turn system

---
+++ Assets/Scripts/TurnSystem.cs
@@ -0,0 +1,54 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnSystem : MonoBehaviour
+{
+    public bool isYourTurn;
+    public int yourTurn;
+    public int isOpponentTurn;
+    public TextMeshProUGUI turnText;
+
+    public int maxMana;
+    public int currentMana;
+    public TextMeshProUGUI manaText;
+    void Start()
+    {
+        isYourTurn = true;
+        yourTurn = 1;
+        isOpponentTurn = 0;
+
+        maxMana = 1;
+        currentMana = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isYourTurn == true)
+        {
+            turnText.text = "Your turn";
+        }
+        else
+        {
+            turnText.text = "Opponents turn";
+        }
+
+        manaText.text = currentMana + "/" + maxMana;
+    }
+
+    public void EndYourTurn()
+    {
+        isYourTurn = false;
+        isOpponentTurn += 1;
+    }
+
+    public void EndOpponentTurn()
+    {
+        isYourTurn = true;
+        yourTurn += 1;
+
+        maxMana += 1;
+        currentMana = maxMana;
+    }
+}
