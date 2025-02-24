c959f989bd01100a49c7d49d1f27c211eebed497
cat <c.finley663@mybvc.ca>
2025-02-24 14:32:03 -0700

    troubleshooting display issues

---
+++ Assets/Scripts/CardToHand.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CardToHand : MonoBehaviour
+{
+    public GameObject Hand;
+    public GameObject HandCard;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        Hand = GameObject.Find("Hand");
+        HandCard.transform.SetParent(Hand.transform);
+        HandCard.transform.localScale = Vector3.one;
+        HandCard.transform.position = new Vector3(transform.position.x, transform.position.y, -48);
+        HandCard.transform.eulerAngles = new Vector3(25, 0, 0);
+    }
+}
